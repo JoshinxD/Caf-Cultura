80b9b349bc3d76583ece8720ab84d82671ec3f5c
JoshinxD <joshuadsanabrial@gmail.com>
2024-06-28 18:15:52 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+<section id="about">
+    <h2>Sobre Nosotros</h2>
+    <img src="ruta/a/tu/imagen.jpg" alt="Ambiente de CaféCultura">
+    <p>Un lugar acogedor con decoración artística, música suave, y rincones para leer o trabajar.</p>
+</section>
